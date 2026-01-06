6e6fa94ae800daddf54e09f22b98d90bfd2f2fd0
5d <d5@mypc.localdomain>
2026-01-06 22:28:55 +0800

    dlco: lab6: add lfsr segment show funtion

--- e_phase/dlco/lab6/vsrc/top.v
+++ e_phase/dlco/lab6/vsrc/top.v
@@ -2,10 +2,19 @@ module top(
     input clk,
     input [7:0] x,
     input reset,
-    output [7:0] y
-
+    output [7:0] y,
+    output [6:0] seg0,
+    output [6:0] seg1
 );
+
+    wire [3:0] bcd0;
+    wire [3:0] bcd1;
+
     lsfr lsfr0(clk, x, reset, y);
 
-    // todo: show byte use bcd.    
+    byte2bcd byte2bcd0(y, bcd0, bcd1);
+    
+    bcd7seg bcd7seg0(bcd0, seg0);
+    bcd7seg bcd7seg1(bcd1, seg1);
+
 endmodule   
